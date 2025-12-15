296c6a4a66a1e2f41068ae0821625c55df75c0eb
clown <llkwork@mail.ru>
2025-12-15 07:28:25 +0300

    Stage 3. + new Dump(spring.autocomfigure.exclude)

--- src/main/java/org/example/apionlinebanking/ApiOnlineBankingApplication.java
+++ src/main/java/org/example/apionlinebanking/ApiOnlineBankingApplication.java
@@ -3,6 +3,7 @@ package org.example.apionlinebanking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ApiOnlineBankingApplication {
 
